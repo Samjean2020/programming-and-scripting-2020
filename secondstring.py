e8710d51000d9f5895adb30de2e41ee929f37edd
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-01 11:33:15 +0000

    Add files via upload

---
+++ secondstring.py
@@ -0,0 +1,7 @@
+#This is a program that takes asks a user to input a string and outputs every second letter in reverse order
+print("Gender equality matters.")
+gem = "gender equality matters."
+len(gem)
+print(len(gem))
+print(gem[0:24])
+print(gem[-1:-24:-2])
