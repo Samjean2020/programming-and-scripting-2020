0f0acb6d146956d13b2ed4294a479877714c9e2f
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-11 20:30:44 +0000

    Delete secondstring.py

--- secondstring.py
+++
@@ -1,9 +0,0 @@
-#This is a program that takes asks a user to input a string and outputs every second letter in reverse order
-print("Gender equality matters.")
-gem = "gender equality matters."
-len(gem)
-print(len(gem))
-print(gem[0:24])
-print(gem[-1:-24:-2])
-
-.rta tluerde
