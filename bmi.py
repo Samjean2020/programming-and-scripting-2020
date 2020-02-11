bc9db79463db111d1125ddd23f645db11cd4ed1d
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-11 20:11:24 +0000

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 # This program calculates an individual's Body Mass Index (BMI) in metres squared
-Weight = 80
+Weight = 65
 Height = 180
 
 BMI = Weight / Height
 print("body mass index in metres squared")
-BMI = 24.69
+BMI = 20.06
