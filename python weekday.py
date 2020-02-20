6e4a162a2c514bfb3ee13b5c735b32a51e1a5649
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-20 16:33:44 +0000

    Add files via upload

---
+++ python weekday.py
@@ -0,0 +1,22 @@
+# Jean Bonsenge
+# This program outputs whether or not today is a weekday.
+weekdays = (" Monday " , 
+            " Tuesday " ,
+            " Wednesday ", 
+            " Thursday " ,
+            " Friday " ,
+            " Saturday " ,
+            " Sunday " )
+
+weekday = weekdays [ 2 : 4 ]
+for weekday in weekdays :
+    print (weekday)
+
+print (" Are you available for a meeting today? ")
+print (" Yes, unfortunately it is weekday.")
+
+weekend = weekdays [ 4 : 6 ]
+for weekend in weekdays :
+    print ( weekend )
+print (" Are you available for a picnic today? ")
+print (" It is the weekend, Yay! ") 
