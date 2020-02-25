4e62f8add8138bc019910652239077a0abb14541
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-25 17:22:12 +0000

    Update python weekday.py

--- python weekday.py
+++ python weekday.py
@@ -8,14 +8,14 @@ weekdays = (" Monday " ,
             " Saturday " ,
             " Sunday " )
 
-weekday = weekdays [ 2 : 4 ]
+weekday = weekdays [2 : 4]
 for weekday in weekdays :
     print (weekday)
 
 print (" Are you available for a meeting today? ")
 print (" Yes, unfortunately it is weekday.")
 
-weekend = weekdays [ 4 : 6 ]
+weekend = weekdays [4 : 6]
 for weekend in weekdays :
     print (weekend)
 print (" Are you available for a picnic today? ")
