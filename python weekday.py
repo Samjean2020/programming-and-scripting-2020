2eabde0f9ab98a6e22d8eb16dccd79e0c5625ff7
Samjean2020 <jeansamuel1@hotmail.com>
2020-02-25 17:23:12 +0000

    .

---
+++ python weekday.py
@@ -0,0 +1,19 @@
+# Jean Bonsenge
+# This program outputs whether or not today is a weekday.
+weekdays = ( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
+
+weekday = weekdays [3]
+
+for weekday in weekdays :
+    print (weekday, end= ' ' )
+
+print (" Are you available for a meeting today? ")
+print ( (weekdays [3]) )
+print (" Yes, unfortunately it is weekday.")
+
+weekend = weekdays [5]
+for weekend in weekdays :
+    print ( weekend )
+print ( " are you available for a picnic today? ")
+print ( " it is the weekend, Yay! " ) 
+print ( (weekend [5] )
---
+++ squareroot.py
@@ -0,0 +1,8 @@
+# Jean Bonsenge
+# This program takes a positive floating-point as input
+# and outputs an approximation of its square root.
+import math
+# please enter a positive number:
+pNumber = 14.5 
+q = math.sqrt(pNumber)
+print ('root square of' , pNumber , '=' , math.sqrt(pNumber) )
