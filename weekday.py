86a92e5c88454b6faa7387ed5bdd77dcd9dd6343
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-02-25 19:41:41 +0000

    Add files via upload

---
+++ weekday.py
@@ -0,0 +1,14 @@
+# Jean Bonsenge
+# This program outputs whether or not today is a weekday.
+
+t = ['Thursday']
+s = ['Saturday']
+
+print ('Are you available for a meeting today?')
+print (t)
+print ('Yes, unfortunately it is weekday.')
+
+print ('are you available for a picnic today?')
+print (s)
+
+print ('it is the weekend, Yay!') 
