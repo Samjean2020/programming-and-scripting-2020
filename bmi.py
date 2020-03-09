aea7e6d746aee0b14ec2b0b968dfdca15dfedf0c
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-09 13:11:17 +0000

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,22 @@
+# This program calculates an individual's Body Mass Index (BMI) in metres squared
+
+import math
+
+# Please enter a person's weight:
+pWeight = 65
+print (" Enter weight: ", pWeight)
+
+# Please enter a person's height:
+pHeight = 180
+print (" Enter height: ", pHeight)
+
+# Please enter a person's height in metres squared:
+sqmpHeight = 1.8
+
+BMI = pWeight / sqmpHeight ** 2
+
+print ("BMI is", BMI)
+
+
+
+
