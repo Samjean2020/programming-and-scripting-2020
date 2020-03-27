816650f7937b36ba8b139ec4a439a85a00efb63b
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-27 14:40:16 +0000

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,3 +1,4 @@
+# Jean Bonsenge
 # This program calculates an individual's Body Mass Index (BMI).
 # The inputs are the person's height in Centimetres, and weight in Kilograms.
 # The output is the weight divided by height in metres squared.
