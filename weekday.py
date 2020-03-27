1d319e0cc7f6aca538f41ba50cfc46102e094612
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-27 14:15:39 +0000

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -13,7 +13,7 @@ def dayNameFromWeekday(weekday):
 nb = int(input("Enter weekday number [0-4]: "))
 print('\n')
 
-print("Today is: " + dayNameFromWeekday(nb), "Yes, unfortunately today is a weekday")
+print("Today is: " + dayNameFromWeekday(nb), "Yes, unfortunately today is a weekday.")
 
 """ I found part of the above code at https://rextester.com/OPSU70132 """
 
