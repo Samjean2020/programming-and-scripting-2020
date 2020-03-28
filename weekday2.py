8e187e187dec5a4cfaecb41b4a090757ac146f1e
Samjean2020 <jeansamuel1@hotmail.com>
2020-03-28 22:20:14 +0000

    .

---
+++ weekday2.py
@@ -0,0 +1,28 @@
+# Jean Bonsenge
+
+# This program outputs whether or not today is a weekday.
+
+import datetime
+
+days = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4: "Friday", 5:"Saturday", 6:"Sunday"}
+
+def dayNameFromWeekday(weekday):
+    if weekday > 5:
+        print("error")
+        return" no running of the program today."
+
+    if weekday < 5:
+
+        print("error")
+        return" no running of the program today."
+    return days[weekday]
+
+nb = int(input("Enter weekday number: "))
+
+print('\n')
+
+print(dayNameFromWeekday(nb) + (": It is the weekend, yay!")) 
+
+"""I found part of the above code at https://rextester.com/OPSU70132"""
+
+
