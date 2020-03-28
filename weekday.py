0333d8399ade3d18de824c8116b4049929f01bcd
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-28 21:22:55 +0000

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -1,3 +1,5 @@
+# Jean Bonsenge
+
 # This program outputs whether or not today is a weekday.
 
 import datetime
@@ -5,17 +7,24 @@ import datetime
 days = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4: "Friday", 5:"Saturday", 6:"Sunday"}
 
 def dayNameFromWeekday(weekday):
-    if weekday > 4:
+    if weekday > 3:
         print("error")
-        return" an unknown day"
+        return" no running of the program today."
+
+    if weekday < 3:
+
+        print("error")
+        return" no running of the program today."
     return days[weekday]
 
-nb = int(input("Enter weekday number [0-4]: "))
+nb = int(input("Enter weekday number: "))
+
 print('\n')
 
-print("Today is: " + dayNameFromWeekday(nb), "Yes, unfortunately today is a weekday.")
+print(dayNameFromWeekday(nb) + (": Yes, unfortunately today is a weekday.")) 
 
-""" I found part of the above code at https://rextester.com/OPSU70132 """
+"""I found part of the above code at https://rextester.com/OPSU70132"""
+
 
 
 
