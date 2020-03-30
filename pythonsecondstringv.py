d9b435995278ebc3b39f978e6352762fa58042ad
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-30 22:00:07 +0100

    Update pythonsecondstringv.py

--- pythonsecondstringv.py
+++ pythonsecondstringv.py
@@ -2,8 +2,11 @@
 # This is a program that takes asks a user to input a string and
 # outputs every second letter in reverse order.
 
+# provide a text sequence type: string. 
 s = str(input("Please enter a sentence: "))
 
+# the length of the string is:44. Obtained using built-in function len() function.
+# start from end of the string and output every second letter.
 print(s[-1:-44:-2])
 
 
