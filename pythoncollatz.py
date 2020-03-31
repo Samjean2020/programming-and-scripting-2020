95b741438db8fd7cbc3fd2b460bdefda1b37d397
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-03-31 17:54:41 +0100

    Update pythoncollatz.py

--- pythoncollatz.py
+++ pythoncollatz.py
@@ -5,9 +5,11 @@
 # if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
 # Have the program end if the current value is one. 
 
+# input a whole number value
 y = int (input(" Enter any positive integer number: "))
+# divide to segregate even (modulus with remainder 0) and odd numbers (with the remainder 1).
 z = 2
-
+# a while loop to loop over until set of Boolean conditions are met.
 while y > 1:
    if y % z == 0:
        y = y/2
@@ -15,7 +17,7 @@ while y > 1:
    else:
        y = y * 3 + 1
        print(y)
-  
+ # display the results of the function print() 
 print()
 
 
