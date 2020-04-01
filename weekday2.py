bced12b6104d6ee50146b46c91585e100deb774e
Samjean2020 <60227439+Samjean2020@users.noreply.github.com>
2020-04-01 14:51:04 +0100

    Update weekday2.py

--- weekday2.py
+++ weekday2.py
@@ -1,11 +1,17 @@
 # Jean Bonsenge
 
 # This program outputs whether or not today is a weekday.
-# This program is ruuning on a Saturday.
+# This program is running on a Saturday.
 
+# Import Python's datetime module.
 import datetime
 
+# Make array (a dictionary) of all the days in order.
+# Have indexed array items starting at 0 to Monday, ending with 6 to Sunday.
 days = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4: "Friday", 5:"Saturday", 6:"Sunday"}
+
+# Define a function for weekday and set up an if loop, 
+# to loop over until the set Boolean conditions are completed.
 
 def dayNameFromWeekday(weekday):
     if weekday > 5:
@@ -18,10 +24,12 @@ def dayNameFromWeekday(weekday):
         return" no running of the program today."
     return days[weekday]
 
+# Get keyboard input, but convert it to integer.
 nb = int(input("Enter weekday number: "))
-
+# display a new line.
 print('\n')
 
+# Print the results of the function.
 print(dayNameFromWeekday(nb) + (": It is the weekend, yay!")) 
 
 """I found part of the above code at https://rextester.com/OPSU70132"""
